424325c85759f71d1d0d121c3b6cc973c6be5042
disris <baironvillarberrios@gmail.com>
2025-05-12 12:42:02 -0400

    fixed host

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const productsApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/productos/'
+    baseURL: 'http://localhost:8000/api/productos/'
 })
 
 export const getProducts = () => productsApi.get()
