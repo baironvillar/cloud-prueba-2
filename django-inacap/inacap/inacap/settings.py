6de680360c7395627b3e4c7a8cb4e3e2fb904b5c
disris <baironvillarberrios@gmail.com>
2025-05-12 12:12:08 -0400

    add * to ALLOWED_HOSTS

--- django-inacap/inacap/inacap/settings.py
+++ django-inacap/inacap/inacap/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-6n*v)xbxno!js13@u(abcx^&)46v(6umv-sph@hxa2j1uax1zu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
