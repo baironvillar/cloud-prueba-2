3222d79e5b2f0e81c713cb9becbbd01496ef38b6
disris <baironvillarberrios@gmail.com>
2025-05-13 01:44:25 -0400

    fix ip

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -1,11 +1,22 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const productsApi = axios.create({
-    baseURL: 'http://localhost:8000/api/productos/'
-})
+let productsApi; // Declaramos pero no inicializamos aún
 
-export const getProducts = () => productsApi.get()
-export const getProduct = (id) => productsApi.get(`${id}`)
-export const createProduct = (product) => productsApi.post('/', product)
-export const updateProduct = (id, product) => productsApi.put(`/${id}/`, product)
-export const deleteProduct = (id) => productsApi.delete(`/${id}/`)
+// Función para configurar la base URL del backend
+async function setBaseURL() {
+  const baseURL = `http://${window.location.hostname}:8000/api/productos/`;
+
+  // Inicializamos productsApi con la URL correcta
+  productsApi = axios.create({
+    baseURL,
+  });
+}
+
+// Asegúrate de que se ejecute antes de cualquier llamada a la API
+await setBaseURL();
+
+export const getProducts = () => productsApi.get();
+export const getProduct = (id) => productsApi.get(`${id}`);
+export const createProduct = (product) => productsApi.post('/', product);
+export const updateProduct = (id, product) => productsApi.put(`/${id}/`, product);
+export const deleteProduct = (id) => productsApi.delete(`/${id}/`);
